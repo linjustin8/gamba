af1bd906767a9fea9ac7bbe9f1248095ae75dd1d
justin <justinlin@ufl.edu>
2023-09-04 10:43:03 -0400

    set up buttons

---
+++ src/button.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+//"PLAY" Button
+export function playButton() 
+{
+    const handleClick = () =>
+    {
+        //logic for handleClick
+    }
+
+    return
+    (
+        <button onClick={handleClick}>PLAY</button>
+    );
+}
+
+//"HOW TO PLAY" Button
+export function howToButton() 
+{
+    const handleClick = () =>
+    {
+        //logic for handleClick
+    }
+
+    return
+    (
+        <button onClick={handleClick}>HOW TO PLAY</button>
+    );
+}
+
+//"SETTINGS" Button
+export function settingsButton() 
+{
+    const handleClick = () =>
+    {
+        //logic for handleClick
+    }
+
+    return
+    (
+        <button onClick={handleClick}>SETTINGS</button>
+    );
+}
+
+//"LEADERBOARD" Button
+export function leaderboardButton() 
+{
+    const handleClick = () =>
+    {
+        //logic for handleClick
+    }
+
+    return
+    (
+        <button onClick={handleClick}>LEADERBOARD</button>
+    );
+}
---
+++ src/start.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import {playButton, howToButton, settingsButton, leaderboardButton} from './button'
+
