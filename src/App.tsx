6d2eaf8bd93ff8f0e9c89acafddbc32aa4df6cbe
justin <justinlin@ufl.edu>
2023-09-07 08:06:21 -0400

    commit for melvin help

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
 //App.tsx
 
 import React from 'react';
+import Start from './components/Start';
 import './App.css'
-import {Button} from './components/StartButtons'
-
 
 
 
 
 function App() {
 
-    const clickPlay = () => {
-        console.log("balls");
-    }
-
-    return (
-    <div className="btn_container">
-        <Button text="PLAY" onClickHandler={clickPlay} />
-        <Button text ="HOW TO PLAY" onClickHandler={clickPlay} />
-        <Button text ="LEADERBOARD" onClickHandler={clickPlay} />
-        <Button text ="SETTINGS" onClickHandler={clickPlay} />
-        <Button text ="ABOUT" onClickHandler={clickPlay} />
-    </div>
-    );
+    
 }
 
 export default App;
--- src/components/start.tsx
+++ src/components/start.tsx
@@ -1,3 +1,24 @@
-import React from 'react';
-import {PlayButton, HowToButton, SettingsButton, LeaderboardButton} from './StartButtons'
+//Start.tsx
 
+import React from 'react';
+import {Button} from './StartButtons';
+
+
+function Start() {
+    const clickPlay = () => {
+        console.log("balls");
+    }
+
+    return (
+        <div className="btn_container">
+            <Button text="PLAY" onClickHandler={clickPlay} />
+            <Button text ="HOW TO PLAY" onClickHandler={clickPlay} />
+            <Button text ="LEADERBOARD" onClickHandler={clickPlay} />
+            <Button text ="SETTINGS" onClickHandler={clickPlay} />
+            <Button text ="ABOUT" onClickHandler={clickPlay} />
+        </div>
+    );
+}
+
+
+export default Start;
